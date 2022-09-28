9ea77f8442bec8f7e7ff49e424e853491f68715d
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-09-27 21:47:54 -0600

    working tables commit

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const Post = require('./post');
 const Bookmark = require('./bookmark');
 const Tag = require('./tag');
 
-User.hasMany(Post, Bookmark, Tag, {
+User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
@@ -13,11 +13,21 @@ Post.belongsTo (User, {
 });
 
 Bookmark.belongsTo (User, {
-    foreignKey: 'user_id'
+    foreignKey: 'post_id'
 });
-
-Tag.belongsTo (User {
-    foreignKey: 'user_id'
+User.hasMany(Bookmark, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
+//belongs to many need through 
+// Post.belongsToMany(Tag, {
+    
+//     foreignKey: 'post_id',
+//     onDelete: 'CASCADE'
+// });
+// Tag.hasMany(Post, {
+//     foreignKey: 'post_id',
+//     onDelete: 'CASCADE'
+// });
 
 module.exports = { User, Post, Bookmark, Tag};
