7e9c2766c5abb0720cfd23bf157b564c5bc8e04d
Wild Rivers <jessicamichele11@gmail.com>
2022-09-27 20:33:58 -0600

    minor html edits and css styling changes

---
+++ public/login/login.js
@@ -0,0 +1,5 @@
+var submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", function () {});
+
+var signupBtn = document.querySelector("#createAccount");
+signupBtn.addEventListener("click", function () {});
