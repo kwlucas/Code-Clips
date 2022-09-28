1b4b9eedacc48893e05820345ab72ef7fbab793b
kwlucas <lucaswkyle@gmail.com>
2022-09-27 19:30:26 -0600

    Delete bookmark method

--- routes/api/bookmarkRoutes.js
+++ routes/api/bookmarkRoutes.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 
 
+router.delete('/:id', async (req, res) => {
+    try {
+      const bookmarkData = await Bookmark.destroy({
+        where: { id: req.params.id }
+      });
+      if (!bookmarkData) {
+        res.status(404).json({ message: 'No bookmark with this id!' });
+        return;
+      }
+      res.status(200).json(bookmarkData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+
 module.exports = router;
