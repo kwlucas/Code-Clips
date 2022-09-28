2fa868b691b02bebc32c478a3b66a01a86934101
kwlucas <lucaswkyle@gmail.com>
2022-09-28 14:56:29 -0600

    pseudo code for bookmark post route

--- routes/api/bookmarkRoutes.js
+++ routes/api/bookmarkRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const { Bookmark } = require('../../models');
 
 router.post('/', async (req, res) => {
+  //body needs to have "userId" and "postId"
+  //userId is from cookie/session data and is the id of who is logged in
+  //postId is from front end regarding which post the bookmark button was linked to
     try {
         const bookmarkData = await Bookmark.create(req.body);
         res.status(200).json(bookmarkData);
