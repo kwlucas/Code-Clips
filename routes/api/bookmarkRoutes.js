4e13eae7bed892b916cc4c1c58151a537b4e9b9f
kwlucas <lucaswkyle@gmail.com>
2022-09-27 19:34:43 -0600

    created bookmark post method

--- routes/api/bookmarkRoutes.js
+++ routes/api/bookmarkRoutes.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
+const { Bookmark } = require('../../models');
 
+router.post('/', async (req, res) => {
+    try {
+        const bookmarkData = await Bookmark.create(req.body);
+        res.status(200).json(bookmarkData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 router.delete('/:id', async (req, res) => {
     try {
