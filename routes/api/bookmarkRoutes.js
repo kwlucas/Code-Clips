a887bd3ab9d18218dac101c4c014ba0404c13af6
kwlucas <lucaswkyle@gmail.com>
2022-09-29 16:56:44 -0600

    Bookmark route adjustments and testing

--- routes/api/bookmarkRoutes.js
+++ routes/api/bookmarkRoutes.js
@@ -5,50 +5,76 @@ router.post('/', async (req, res) => {
   //body needs to have "user_id" and "post_id"
   //user_id is from cookie/session data and is the id of who is logged in
   //post_id is from front end regarding which post the bookmark button was linked to
-    try {
-      if(req.body.user_id && req.body.post_id){
-        res.status(400).send({message: 'Invalid bookmark parameters!'});
-      }
-        const bookmarkData = await Bookmark.create(req.body);
-        res.status(200).json(bookmarkData);
-    } catch (err) {
-        res.status(400).json(err);
+  try {
+    if (req.body.user_id && req.body.post_id) {
+      const bookmarkData = await Bookmark.create(req.body);
+      res.status(200).json(bookmarkData);
+    } else {
+      res.status(400).send({ message: 'Invalid bookmark parameters!' });
     }
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 //get all the bookmarks belonging to specificed user
-router.get('/:user_id', async (req, res) => {
-    try {
-        const bookmarkData = await Bookmark.findAll({
-          where: {
-            user_id: req.params.user_id
-          },
-          attributes: {
-            //Fields which won't be included in response data
-            exclude: ['id', 'user_id']
-          }
-        });
+router.get('/u/:user_id', async (req, res) => {
+  try {
+    const bookmarkData = await Bookmark.findAll({
+      where: {
+        user_id: req.params.user_id
+      },
+      attributes: {
+        //Fields which won't be included in response data
+        exclude: ['id', 'user_id']
+      }
+    });
+    res.status(200).json(bookmarkData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//delete the bookmarks beloning to the specified user on the specified post
+router.delete('/', async (req, res) => {
+  try {
+    if (req.body.user_id && req.body.post_id) {
+      const bookmarkData = await Bookmark.destroy({
+        where: {
+          user_id: req.body.user_id,
+          post_id: req.body.post_id
+        },
+      });
+      if (!bookmarkData) {
+        res.status(404).json({ message: 'No bookmark with these parameters!' });
+
+      } else {
         res.status(200).json(bookmarkData);
-    } catch (err) {
-        res.status(400).json(err);
+      }
+    } else {
+      res.status(400).send({ message: 'Invalid bookmark parameters!' });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Delete the bookmark with specified id
 router.delete('/:id', async (req, res) => {
-    try {
-      const bookmarkData = await Bookmark.destroy({
-        where: { id: req.params.id }
-      });
-      if (!bookmarkData) {
-        res.status(404).json({ message: 'No bookmark with this id!' });
-        return;
-      }
+  try {
+    const bookmarkData = await Bookmark.destroy({
+      where: { id: req.params.id }
+    });
+    if (!bookmarkData) {
+      res.status(404).json({ message: 'No bookmark with this id!' });
+
+    } else {
       res.status(200).json(bookmarkData);
-    } catch (err) {
-      res.status(500).json(err);
     }
-  });
-  
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
