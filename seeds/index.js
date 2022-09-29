8bb076d6041d730bc211a1845253291d33047623
kwlucas <lucaswkyle@gmail.com>
2022-09-29 15:46:58 -0600

    Adjusted logging for seeding

--- seeds/index.js
+++ seeds/index.js
@@ -4,11 +4,11 @@ const seedBookmarks = require('./bookmark-seeds');
 const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\nDB Sync');
+  console.log('\nDB Sync Complete\nSeeding Users');
   await seedUsers();
-  console.log('\nUsers Seeded');
+  console.log('\nUsers Seeded\nSeeding Posts');
   await seedPosts();
-  console.log('\nPosts Seeded');
+  console.log('\nPosts Seeded\nSeeding Bookmarks');
   await seedBookmarks();
   console.log('\nBookmarks Seeded');
   process.exit(0);
