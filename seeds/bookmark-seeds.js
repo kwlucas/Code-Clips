4980aa85ac7dec9520db4ac3950c9de240c83aec
kwlucas <lucaswkyle@gmail.com>
2022-09-28 20:53:14 -0600

    entered bookmark seed data

--- seeds/bookmark-seeds.js
+++ seeds/bookmark-seeds.js
@@ -1,7 +1,34 @@
 const { Bookmark } = require('../models');
 
 const bookmarkData = [
-    {}
+    {
+        user_id: 1,
+        post_id: 1,
+    },
+    {
+        user_id: 2,
+        post_id: 2,
+    },
+    {
+        user_id: 2,
+        post_id: 3,
+    },
+    {
+        user_id: 3,
+        post_id: 1,
+    },
+    {
+        user_id: 3,
+        post_id: 2,
+    },
+    {
+        user_id: 3,
+        post_id: 3,
+    },
+    {
+        user_id: 4,
+        post_id: 3,
+    },
 ]
 
 const seedBookmarks = () => Bookmark.bulkCreate(bookmarkData);
