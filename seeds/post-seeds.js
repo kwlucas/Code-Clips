8e18a9b9cdb4ecd76f03cd570f00fcbb404e4127
kwlucas <lucaswkyle@gmail.com>
2022-09-28 21:31:02 -0600

    Post seeds causing problems NEED debugging

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,25 +2,28 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        user_id: 1,
         title: 'Sequelize Relationship',
-        snippet: 'ModelA.hasMany(ModelB, {\n\tforeignKey: "modelA_id",\n\tonDelete: "CASCADE"\n});\n\nModelB.belongsTo(ModelA, {\n\tforeignKey: "modelA_id"\n});',
-        description: 'A sequalize one to many relationship.'
+        snippet: 'Post one snippet',
+        description: 'A sequalize one to many relationship.',
+        user_id: 1
     },
     {
-        user_id: 2,
         title: 'HTML code block',
-        snippet: '<pre>\nContents of the code block.\n</pre>',
-        description: 'Content within the pre tags will bre presented nearly exactly as written and in monospace.\nPerfect for code blocks!'
+        snippet: '<pre>Contents of the code block.</pre>',
+        description: 'Content within the pre tags will bre presented nearly exactly as written and in monospace. Perfect for code blocks!',
+        user_id: 2
     },
     {
-        user_id: 4,
         title: 'Highlight.js on node',
-        snippet: 'hljs = require(\'highlight.js\');\ndocument.querySelectorAll(".classYouWantHighlighted").foreach(el => {\n\thljs.highlightElement(el);\n});',
-        description: 'This is for a basic syntax highlighting of HTML elements with a certain selector/class.'
-    },
+        snippet: 'Post three snippet',
+        description: 'This is for a basic syntax highlighting of HTML elements with a certain selector/class.',
+        user_id: 3
+    }
 ]
 
 const seedPosts = () => Post.bulkCreate(postData);
 
 module.exports = seedPosts;
+
+//'ModelA.hasMany(ModelB, {\n\tforeignKey: "modelA_id",\n\tonDelete: "CASCADE"\n});\n\nModelB.belongsTo(ModelA, {\n\tforeignKey: "modelA_id"\n});',
+//'hljs = require(\'highlight.js\');\ndocument.querySelectorAll(".classYouWantHighlighted").foreach(el => {\n\thljs.highlightElement(el);\n});',
