324e887957b571836d02ad6db8b31e513fe5c14e
kwlucas <lucaswkyle@gmail.com>
2022-09-28 18:30:58 -0600

    Added get method for bookmarks

--- routes/api/bookmarkRoutes.js
+++ routes/api/bookmarkRoutes.js
@@ -2,11 +2,28 @@ const router = require('express').Router();
 const { Bookmark } = require('../../models');
 
 router.post('/', async (req, res) => {
-  //body needs to have "userId" and "postId"
+  //body needs to have "user_id" and "post_id"
   //userId is from cookie/session data and is the id of who is logged in
   //postId is from front end regarding which post the bookmark button was linked to
     try {
         const bookmarkData = await Bookmark.create(req.body);
+        res.status(200).json(bookmarkData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const bookmarkData = await Bookmark.findAll({
+          where: {
+            user_id: req.body.user_id
+          },
+          attributes: {
+            //Fields which won't be included in response data
+            exclude: ['user_id']
+          }
+        });
         res.status(200).json(bookmarkData);
     } catch (err) {
         res.status(400).json(err);
