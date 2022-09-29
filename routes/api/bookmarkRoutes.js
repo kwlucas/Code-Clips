ea59759de9f207e8aaaff547bbd53087b95fe4bb
kwlucas <lucaswkyle@gmail.com>
2022-09-28 18:34:27 -0600

    minor code convention fixes

--- routes/api/bookmarkRoutes.js
+++ routes/api/bookmarkRoutes.js
@@ -3,8 +3,8 @@ const { Bookmark } = require('../../models');
 
 router.post('/', async (req, res) => {
   //body needs to have "user_id" and "post_id"
-  //userId is from cookie/session data and is the id of who is logged in
-  //postId is from front end regarding which post the bookmark button was linked to
+  //user_id is from cookie/session data and is the id of who is logged in
+  //post_id is from front end regarding which post the bookmark button was linked to
     try {
         const bookmarkData = await Bookmark.create(req.body);
         res.status(200).json(bookmarkData);
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
           },
           attributes: {
             //Fields which won't be included in response data
-            exclude: ['user_id']
+            exclude: ['id', 'user_id']
           }
         });
         res.status(200).json(bookmarkData);
