9504772ecaac2dd5063c73e37408d7a3d66b3bf3
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-09-29 20:08:29 -0600

    routes error

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,66 @@
 const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        if(req.body.username && req.body.password){
+            const userData = await User.create({
+                username: req.body.username,
+                password: req.body.password,
+            });
+            //session save
+            res.json(userData);
+        } else {
+            res.status(400).send({ message: 'Invalid user parameters!' });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+router.post('/login', async (req, res) => {
+    try {
+      const user = await User.findOne({
+        where: {
+          username: req.body.username,
+        },
+      });
+  
+      if (!user) {
+        res.status(400).json({ message: 'No user account found!' });
+        return;
+      }
+  
+      const validPassword = user.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+          console.log("incorrect");
+        res.status(400).json({ message: 'No user account found!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.userId = user.id;
+        req.session.username = user.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(400).json({ message: 'No user account found!' });
+    }
+  });
+  
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+
 
 
 module.exports = router;
---
+++ seeds/user-seeds.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'brendon@email.com',
+        password: 'password4Brendon'
+    },
+    {
+        username: 'jess@email.com',
+        password: 'password4Jess'
+    },
+    {
+        username: 'jose@email.com',
+        password: 'password4Jose'
+    },
+    {
+        username: 'kyle@email.com',
+        password: 'password4Kyle'
+    },
+];
+
+const seedUsers = function() {userData.forEach(data => User.create(data))};
+
+module.exports = seedUsers;
