b3643ad44b870bdf8218434da0565e40c0ae353a
kwlucas <lucaswkyle@gmail.com>
2022-09-29 18:30:20 -0600

    future development comments

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -4,6 +4,7 @@ const { Post } = require('../../models');
 
 router.post('/', async (req, res) => {
     try {
+        //Before public deployement this MUST have authentication attached to it. Users should only be able to add their own bookmarks
         if (req.body.user_id && req.body.title && req.body.snippet) {
             const postData = await Post.create(req.body);
             res.status(200).json(postData);
