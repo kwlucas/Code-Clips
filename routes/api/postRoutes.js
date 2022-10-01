032620339086de6c950a097f8b27e168a07cb978
kwlucas <lucaswkyle@gmail.com>
2022-10-01 14:35:25 -0600

    Post routes GET / inclides author modles.

--- routes/api/postRoutes.js
+++ routes/api/postRoutes.js
@@ -1,15 +1,41 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Sequelize } = require('sequelize');
+const { Post, User, Bookmark } = require('../../models');
 
 
 router.post('/', async (req, res) => {
     try {
-        //Before public deployement this MUST have authentication attached to it. Users should only be able to add their own bookmarks
+        //Before public deployement this MUST have authentication attached to it. Users should only be able to add their own Posts
         if (req.body.user_id && req.body.title && req.body.snippet) {
             const postData = await Post.create(req.body);
             res.status(200).json(postData);
         } else {
             res.status(400).send({ message: 'Invalid post parameters!' });
+        }
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//SELECT * FROM user LEFT OUTER JOIN bookmark ON user.id = bookmark.user_id;
+//SELECT *,  FROM user LEFT OUTER JOIN bookmark ON user.id = bookmark.user_id;
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {model: User, required: false},
+                //{model: User, through: {},required: false}
+            ],
+            attributes: {
+                //include: [[Sequelize.fn('COUNT', Sequelize.col('bookmar.user_id')), 'bookmark_count']]
+            },
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No Post with these parameters!' });
+
+        } else {
+            res.status(200).json(postData);
         }
     } catch (err) {
         res.status(400).json(err);
