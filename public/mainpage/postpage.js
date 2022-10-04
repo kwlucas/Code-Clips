826b16d84d68b9c3af6d4a115704ba9f32835d38
Jose <joseeenrique.jm@gmail.com>
2022-10-03 19:18:38 -0600

    js working for text area

--- public/mainpage/postpage.js
+++ public/mainpage/postpage.js
@@ -1,6 +1,6 @@
 const addBtn = document.querySelector('.new_note_btn');
 const divPlacement = document.get
-addBtn.addEventListener('click', addNew);;
+addBtn.addEventListener('click', addNew);
 
 function addNew(){
     const newTextArea = document.createElement('textarea');
