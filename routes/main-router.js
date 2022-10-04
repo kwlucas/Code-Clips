0115371dd1c55244559bb1f854ec62c71adf1470
kwlucas <lucaswkyle@gmail.com>
2022-10-03 18:19:15 -0600

    Basic structure for routes

--- routes/main-router.js
+++ routes/main-router.js
@@ -1,4 +1,32 @@
 const router = require('express').Router();
+const { User, Post, Bookmark } = require('../models');
 
+//dashboard/home page
+router.get('/', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+});
+
+//view post page
+router.get('/post/:id', async (req, res) => {
+    try {
+        
+    } catch (err) {
+        
+    }
+});
+
+//login page
+router.get('/login', (req, res) => {
+    
+});
+
+//sign up page
+router.get('/signup', (req, res) => {
+    
+});
 
 module.exports = router;
