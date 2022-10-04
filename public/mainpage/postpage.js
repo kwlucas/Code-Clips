7f37c7b7f4baabed27775c05647999982fccb8fd
Jose <joseeenrique.jm@gmail.com>
2022-10-03 19:18:17 -0600

    new text area working

---
+++ public/mainpage/postpage.js
@@ -0,0 +1,9 @@
+const addBtn = document.querySelector('.new_note_btn');
+const divPlacement = document.get
+addBtn.addEventListener('click', addNew);;
+
+function addNew(){
+    const newTextArea = document.createElement('textarea');
+    newTextArea.classList.add('note-style')
+    document.body.appendChild(newTextArea);
+};
