d4f76da83c677954d3af1eb43022a48e08f0e987
kwlucas <lucaswkyle@gmail.com>
2022-10-04 19:51:03 -0600

    View post functionality

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,10 +1,12 @@
 let addNewBtnEl =  document.querySelector('new-post-btn');
+let viewPostEl = document.querySelector('#view-post');
+
 
 function toggleModal(event){
     const trigger = event.target;
     if(trigger.classList.contains('modal-active')){
         trigger.classList.toggle('modal-active');
-        (document.querySelectorAll('dialog[open]') || []).forEach(modal => {//get all elements with "modal-close" class and run for each
+        (document.querySelectorAll('dialog[open]') || []).forEach(modal => {//get all open modals and run for each
             modal.removeAttribute('open');
         });
         return;
@@ -20,6 +22,34 @@ function toggleModal(event){
         trigger.toggleAttribute('open');
         return;
     }   
+}
+
+async function openPost(event) {
+    const postId = event.target.getAttribute('post_id');
+    openModal('#view-post-modal');
+    addNewBtnEl.classList.add('modal-active');
+    const { id, title, snippet, description, user_id } = await fetch(`api/posts/${postId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+          }
+    });
+    const { username } = await fetch(`api/users/${user_id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+          }
+    });
+    document.querySelector('#post-view-title').textContent = title;
+    document.querySelector('#post-view-author').textContent = username;
+    document.querySelector('#post-view-snippet').textContent = snippet;
+    document.querySelector('#post-view-description').textContent = description;
+}
+
+function refreshPosts(){
+    (document.querySelectorAll('.post[post_id]') || []).forEach(postEl => {//get all elements with "post" class and run for each
+       postEl.addEventListener('click', openPost)
+    });
 }
 
 
