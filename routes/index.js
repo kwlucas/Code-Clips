706e0ecd5e191311eeae75dabe6c7cba5f00f5a2
kwlucas <lucaswkyle@gmail.com>
2022-10-05 15:16:08 -0600

    File movement for routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const mainRoutes = require('./user-router.js');
+const userRoutes = require('./user-router.js');
 const mainRoutes = require('./main-router.js');
 
 router.use('/', mainRoutes);
-router.use('/user', userRoutes);
+//router.use('/user', userRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
