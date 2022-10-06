d5aeeb29bc56ece591ff1d88f5573ce40a633521
kwlucas <lucaswkyle@gmail.com>
2022-10-05 21:37:49 -0600

    highlightJS implemented & working using browserify

---
+++ public/scripts/viewPost.js
@@ -0,0 +1,7 @@
+// const hljs = require('highlight.js/lib/common');
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     (document.querySelectorAll('.highlight-syntax') || []).forEach(element => {
+//         hljs.highlightElement(element);
+//     });
+// })
