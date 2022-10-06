1808948fb18795051f6adde65a08bbb82380579b
kwlucas <lucaswkyle@gmail.com>
2022-10-06 11:46:48 -0600

    Added functionality to copy code button

--- public/scripts/viewPost.js
+++ public/scripts/viewPost.js
@@ -1,5 +1,7 @@
 const addNewBtnEl = document.querySelector('#new-post-btn');
 const modalEl = document.querySelector('#main-modal');
+const snippetEl = document.querySelector('#post-view-snippet');
+const copyBtnEl = document.querySelector('#snippet-copy-btn');
 
 function delay(seconds = 1) {
     return new Promise(resolve => setTimeout(resolve, seconds * 1000));
@@ -13,5 +15,9 @@ async function closeModal() {
     document.location.replace('/');
 }
 
+copyBtnEl.addEventListener('click', function (event) { 
+    event.preventDefault();
+    navigator.clipboard.writeText(snippetEl.textContent) 
+});
 addNewBtnEl.addEventListener('click', closeModal);
 document.documentElement.classList.add('scroll-disabled');
